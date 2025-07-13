99be3600bd697d2e3073c2e166552e59cdee13b3
Julio Torres <julio.torres7@outlook.com>
2025-07-13 13:18:55 -0300

    adicionando exercicios de float

--- exercicios.py
+++ exercicios.py
@@ -1,46 +1,78 @@
+import math
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-x = int(input("valor 1:"))
-y = int(input("valor 1:"))
-z = x + y
+# x = int(input("valor 1:"))
+# y = int(input("valor 2:"))
+# z = x + y
 
-print(z)
+# print(z)
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-x = int(input("valor 1:"))
+# x = int(input("valor 1:"))
 
-z = x % 5
+# z = x % 5
 
-print(z)
+# print(z)
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-x = int(input("valor 1:"))
-y = int(input("valor 1:"))
-z = x * y
+# x = int(input("valor 1:"))
+# y = int(input("valor 2:"))
+# z = x * y
 
-print(z)
+# print(z)
 
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-x = int(input("valor 1:"))
-y = int(input("valor 1:"))
-z = x / y
+# x = int(input("valor 1:"))
+# y = int(input("valor 2:"))
+# z = x / y
 
-print(int(z))
+# print(int(z))
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
 # #### Números de Ponto Flutuante (`float`)
 
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+# x = float(input("valor 1:"))
+# y = float(input("valor 2:"))
+# z = x + y
+# print(z)
+
+
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# x = float(input("valor 1:"))
+# y = float(input("valor 2:"))
+# z = (x + y)/2
+# print(z)
+
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# x = float(input("valor 1:"))
+# y = float(input("valor 2:"))
+# z = x ** 2
+# print(z)
+
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# x = float(input("valor 1:"))
+
+# z = x * (9/5) + 32
+# print(z)
+
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+# x = float(input("valor 1:"))
+
+# z = math.pi * (x**2)
+# print(z)
 
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+x = str(input("valor 1:"))
+
+print(x.upper)
+
+
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
